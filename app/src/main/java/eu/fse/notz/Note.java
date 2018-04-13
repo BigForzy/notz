4a5e9ecf4222bb0a2c1168517a0c4ff0b5fe2d17
Big <bigforzy@hotmail.it>
2018-04-13 18:52:23 +0200

    quattro

--- app/src/main/java/eu/fse/notz/Note.java
+++ app/src/main/java/eu/fse/notz/Note.java
@@ -5,4 +5,39 @@ package eu.fse.notz;
  */
 
 public class Note {
+    private String title,description;
+    private int id;
+    private boolean isShownOnTop;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isShownOnTop() {
+        return isShownOnTop;
+    }
+
+    public void setShownOnTop(boolean shownOnTop) {
+        isShownOnTop = shownOnTop;
+    }
 }
